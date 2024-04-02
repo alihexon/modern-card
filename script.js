6ebfe8b081da91e8fe9b4a5f071df317f3774f54
hexoN <aliakbaripour00@gmail.com>
2024-04-02 12:45:24 +0330

    Made the script check if the vw is 719px or lower and then handle the interactions

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const cards = document.querySelectorAll('.js-slide');
 
-cards.forEach(card => {
-  card.addEventListener('mouseover', () => card.classList.add('slide'));
-  card.addEventListener('mouseout', () => card.classList.remove('slide'));
+function handleMouseInteraction(card) {
+  if (window.innerWidth <= 719) {
+    card.addEventListener('mouseover', () => card.classList.add('slide'));
+    card.addEventListener('mouseout', () => card.classList.remove('slide'));
+  }
+}
 
+cards.forEach(card => {
+  card.addEventListener('mouseover', () => handleMouseInteraction(card));
+  card.addEventListener('mouseout', () => handleMouseInteraction(card));
 });
